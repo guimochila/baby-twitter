fb01ee3e44e30c78f69f83c58d458dc194eb0646
Guilherme <gui.mochila@gmail.com>
2021-12-30 16:24:14 +0100

    feature: Add JSDOC for auth api

--- apps/api/src/routes/auth.js
+++ apps/api/src/routes/auth.js
@@ -5,6 +5,32 @@ const { login } = require('../components/users/user.controllers')
 function getAuthRoutes() {
   const router = express.Router()
 
+  /**
+   * @swagger
+   * /api/auth:
+   *   post:
+   *     summary:  Authenticate user.
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               email:
+   *                 type: string
+   *                 description: The user's email address.
+   *                 example: john@example.com
+   *              password:
+   *                 type: string
+   *                 description:  The user's password
+   *     responses:
+   *       200:
+   *        description: Authenticated
+   *       401:
+   *        description: Not Authenticated
+   *         ...
+   */
   router.post('/', passport.authenticate('local', { session: false }), login)
 
   return router
